f970a7cd3f994417319efcdc4eee7bccae8f3f2c
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-08 05:31:43 -0700

    revamp the entire file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-import hidden_4.pyc
+import hidden_4
 
 
 def main():
-    for name in dir(hidden_4.pyc):
+    for name in dir(hidden_4):
         if not name.startswith('__'):
             print(name)
 
 
-if _name__ == "__main__":
+if __name__ == "__main__":
     main()
