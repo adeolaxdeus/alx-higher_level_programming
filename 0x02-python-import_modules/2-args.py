01cee2f494a2de619bf3e3c2cc5ab335bb6d1241
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-08 04:45:13 -0700

    fix output case error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,14 @@ import sys
 
 
 def main():
-    length = len(sys.argv)
-    if length > 1:
-        print("{} arguments:".format(length - 1))
-        for index in range(1, length):
+    i = len(sys.argv)
+    if i > 1:
+        if (i - 1) == 1:
+            arguments = "argument"
+        else:
+            arguments = "arguments"
+        print("{} {}:".format((i - 1), arguments))
+        for index in range(1, i):
             print("{}: {}".format(index, sys.argv[index]))
     else:
         print("0 arguments.")
