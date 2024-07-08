a8b06e226ca1306a615b10f90995111802f4f90a
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-08 02:56:26 -0700

    how to mane a dynamic script

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    length = len(sys.argv)
+    if length > 1:
+        print("{} arguments:".format(length - 1))
+        for index in range(1, length):
+            print("{}: {}".format(index, sys.argv[index]))
+    else:
+        print("0 arguments.")
+
+
+main()
