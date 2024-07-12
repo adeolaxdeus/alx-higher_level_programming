379252d6972d0520b8f0a78ca95a1e39e526569c
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-12 10:46:00 -0700

    fix the code

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,8 +2,5 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    update = {k: (value if k == key else v) for k, v in a_dictionary.items()}
-    # add key if not present
-    if key not in update:
-        update[key] = value
-    return update
+    a_dictionary[key] = value
+    return a_dictionary
