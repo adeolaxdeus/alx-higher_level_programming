91430408e0c6d57c295430f4d11935121c0ff6fc
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-14 01:15:27 -0700

    it all on God

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,9 +5,10 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     if a_dictionary != {}:
-        a = 0
-        for key in a_dictionary:
-            if a < a_dictionary[key]:
-                a = a_dictionary[key]
-                b = key
-                return b
+        max_key = None
+        max_value = 0
+        for key, value in a_dictionary.items():
+            if max_value < value:
+                max_value = value
+                max_key = key
+        return max_key
