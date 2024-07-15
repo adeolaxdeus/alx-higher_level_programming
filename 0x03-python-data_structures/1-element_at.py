869a698ab47e79edf8b5d11b185a60c7e7761974
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-15 12:37:45 -0700

    Secure access to an element in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    if idx > len(my_list):
+        return None
+    for num in range(len(my_list)):
+        if num == idx:
+            return my_list[num]
