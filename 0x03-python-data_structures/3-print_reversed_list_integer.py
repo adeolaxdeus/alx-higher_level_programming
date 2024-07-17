792c371d92ae00f155e7a80ecb8f0561005a178a
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-17 07:56:11 -0700

    fix input case

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 
 
 def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return None
     length = len(my_list) - 1
     for index in range(length, -1, -1):
         print("{:d}".format(my_list[index]))
