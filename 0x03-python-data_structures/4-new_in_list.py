73b133f59dbaed3d33e72311e936fee052ac65b6
adeolaxdeus <adeolaxdeus@gmail.com>
2024-07-18 04:08:29 -0700

    fix iteration range

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,10 +6,10 @@ def new_in_list(my_list, idx, element):
         return None
     if idx < 0:
         return my_list
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return my_list
     new_list = my_list[:]
-    for num in range(len(new_list) - 1):
-        if num is idx:
+    for num in range(len(new_list)):
+        if num == idx:
             new_list[idx] = element
     return new_list
