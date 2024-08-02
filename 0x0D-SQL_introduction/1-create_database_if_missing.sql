cfba6aa152e7026a8047b2e24fe0cc101ddc306c
adeolaxdeus <adeolaxdeus@gmail.com>
2024-08-01 19:59:17 -0700

    Creates a database

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- This script crates a database hbtn_0c_0 in your MYSQL server.
+-- The result is a new database hbtn_0c_0 in your MYSQL server.
+
+--SQL queries
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
