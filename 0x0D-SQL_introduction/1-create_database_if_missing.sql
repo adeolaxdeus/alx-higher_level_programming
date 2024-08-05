61e4568b643a0f9e3305a171a47476638d1d3b52
adeolaxdeus <adeolaxdeus@gmail.com>
2024-08-05 02:27:40 -0700

    fix syntax error

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,5 @@
 -- This script crates a database hbtn_0c_0 in your MYSQL server.
 -- The result is a new database hbtn_0c_0 in your MYSQL server.
 
---SQL queries
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0
+-- SQL queries
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
