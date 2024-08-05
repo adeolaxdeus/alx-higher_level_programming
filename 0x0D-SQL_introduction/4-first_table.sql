97206ec2b8f55131c80c0e99e7f24a01323988a1
adeolaxdeus <adeolaxdeus@gmail.com>
2024-08-05 03:23:52 -0700

    First table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- This script creates a table in the current database in your My
+-- The result a table with two column id and name.
+
+-- SQL queries.
+CREATE TABLE IF NOT EXISTS first_table ( id INT,
+										 name VARCHAR(256)
+										);
