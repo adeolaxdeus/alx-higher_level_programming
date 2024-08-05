f4a5ce3a92c74808dfbcbdd707093a43ea149f24
adeolaxdeus <adeolaxdeus@gmail.com>
2024-08-05 02:50:15 -0700

    Delete a database

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- This script drop a database hbtn_0c_0 in your MYSQL server.
+-- The result is a database hbtn_0c_0 is deleted in your MYSQL server.
+
+-- SQL queries
+DROP DATABASE IF EXISTS hbtn_0c_0
