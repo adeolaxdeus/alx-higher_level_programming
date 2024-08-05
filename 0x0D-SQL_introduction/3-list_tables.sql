372278d61f3c7e5bf3919f4207578682f01a802d
adeolaxdeus <adeolaxdeus@gmail.com>
2024-08-05 03:06:04 -0700

    List tables

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- This script that lists all the tables of a database in your MySQL server.
+-- The result is a list of all tables of a database in your MYSQL server
+
+-- SQL queries
+SHOW TABLES;
