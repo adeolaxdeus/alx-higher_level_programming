c616cd0de9a8ef44ca81254cca4a48c2394ffd26
adeolaxdeus <adeolaxdeus@gmail.com>
2024-11-17 13:44:03 -0800

    make file exe, fix pep8 error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-print("{}".format(''.join(chr(c) for c in range(97, 123) \
+print("{}".format(''.join(chr(c) for c in range(97, 123)
                   if chr(c) not in 'eq')), end="")
