d870fe278ad5eecbc979e7768914f571aecd89a5
adeolaxdeus <adeolaxdeus@gmail.com>
2024-11-19 05:56:15 -0800

    x2 combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for num1 in range(0, 10):
-    for num2 in range((num1 + 1), 10):
-        if num1 == 8 and num2 == 9:
-            print("{}{}".format(num1, num2))
-        else:
-            print("{}{}".format(num1, num2), end=", ")
+for i in range(0, 10):
+    for j in range(0, 10):
+        if (j > i):
+            print("{}{}".format(i, j), end="")
+            if (i != 8 or j != 9):
+                print(", ", end="")
+            else:
+                print()
