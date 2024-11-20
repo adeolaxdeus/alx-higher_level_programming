c94103a8c6ecc42027ce1b39bb8e1c6b3dff41ac
adeolaxdeus <adeolaxdeus@gmail.com>
2024-11-20 01:01:21 -0800

    fix pep8 eror

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     string = ""
     for char in str:
         if ('a' <= char <= 'z'):
-            string  += chr(ord(char) - 32)
+            string += chr(ord(char) - 32)
         else:
             string += char
     print(string)
